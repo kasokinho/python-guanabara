4cd0ad96612c65cafb074f1f2d5e99f711e9e309
Kasokinho <alvarogfn@gmail.com>
2021-02-25 12:41:35 -0300

    ex007 - Media aritmetica

---
+++ mundo-1/ex007/ex007.py
@@ -0,0 +1,4 @@
+nota1 = float(input('Digite a 1ª nota do Aluno: '))
+nota2 = float(input('Digite a 2º nota do Aluno: '))
+
+print(f'A media entre as notas {nota1} e {nota2} é {(nota1+nota2)/2:.2f}')
